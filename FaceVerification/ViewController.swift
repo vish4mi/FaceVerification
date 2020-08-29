6133b245661ff119d69b4ef52c4516ccead382d1
Vishal Bhadade <vishal.bhadade@olacabs.com>
2020-08-29 20:02:23 +0530

    Initial Commit

---
+++ FaceVerification/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FaceVerification
+//
+//  Created by Vishal Bhadade on 29/08/20.
+//  Copyright © 2020 VB. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
